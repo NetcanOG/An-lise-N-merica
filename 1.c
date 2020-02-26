3a2ee251b72c205e0d3f875b978c52557b36b9d5
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-02-26 11:32:09 +0000

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int* fat_boost;
+
+int fat(int n){
+   if(fat_boost[n]!=0) return fat_boost[n];
+   fat_boost[n] = n*fat(n-1);
+   return fat_boost[n];
+}
+
+void build_fatBoost(int n){
+   fat_boost = (int *)malloc(2*k*sizeof(int));
+   for(int i=1; i<2*k; i++) fat_boost[i] = 0;
+   fat_boost[0] = 1;
+}
+
+int main(){
+   int k;
+   
+   build_fatBoost(k);
+   
+}
