2bef79011399897b7848f7abe2162756f85b96ff
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-03-04 18:47:31 +0000

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,28 +1,27 @@
 //clear && gcc -Werror -o 2 2.c -lm && ./2 && rm 2
+//calcula sem underflow 10^-9
 
 #include <stdio.h>
 
 int main(){
-   double sum=0.0,temp,sol,epsilon=1.0;
+   double sum=1.0,temp,sol,epsilon=1.0;
    int precisao,k;
 
-   printf("intruduza a precisao.\n10^-");
+   printf("Introduza a precisao.\n10^-");
    scanf("%d",&precisao);
-   for(int i=0; i<precisao; i++) epsilon *=(double) 1.0/10.0;
+   for(k=0; k<precisao; k++) epsilon *=(double) 1.0/10.0;
 
    for(k=2;;k++){
       temp = (double) (1.0/(k*k));
-      printf("%.15f\n",temp);
-
       if(temp < epsilon) break;
-
+      //printf("%.15f > %.15f\n",temp,epsilon);
       if(k%2 == 0) sum+=temp;
       else sum-=temp;
    }
+   //printf("%.52f < %.52f\n",temp,epsilon);
+   
+   printf("\nN de termos somados = %d\n",--k);
 
-   printf("\ntermos somados %d\n", k);
-
-   sol= sum; //* 12;
-
-   printf("%.15f\n",sol);
+   sol= sum * 12;
+   printf("Valor final = %.*f +- 10^-%d\n",precisao+1,sol,precisao);
 }
