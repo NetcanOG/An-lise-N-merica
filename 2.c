21d07a5532fcf4a9c3b32b84aff21a4a41c51ca9
arcmarqs <maramadeu@gmail.com>
2020-03-04 12:33:46 +0000

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,25 +1,38 @@
 #include <stdio.h>
 
 int main(){
-   int k=1,precisao;
-   double sum=0,temp,sol,epsilon;
+int k;
+double sum=1.0,temp,sol,epsilon=1.0;
+int precisao;
 
-   printf("intruduza a precisao.\n10^-");
-   scanf("%d",&precisao);
-   for(int i=0; i<precisao; i++) epsilon *=(double) 1/10;
-      
-   for(k=1;;k++){
-      temp= (double) (1/(k*k));
-      printf("%f",temp);
-      if(temp<epsilon) break;
-      if(k%2 == 0) sum-=temp;
-      else sum+=temp;
-   }
+printf("intruduza a precisao.\n10^-");
+  scanf("%d",&precisao);
 
-   printf("termos somados %d\n", k);
 
-   sol=12 * sum;
+  for(int i=0; i<precisao; i++)
+    epsilon *=(double) 1.0/10.0;
 
-   printf("%.15f\n",sol);
-   return 0;
+
+for(k=2;;k++){
+
+    temp = (double) (1.0/(k*k));
+    printf("%.15f\n",temp);
+
+    if(temp < epsilon)
+      break;
+
+      else{
+     if(k%2 == 0)
+        sum-=temp;
+        else
+          sum+=temp;
+}
+}
+
+printf("termos somados %d\n", k);
+
+sol=12 * sum;
+
+printf("%.15f\n",sol);
+return 0;
 }
