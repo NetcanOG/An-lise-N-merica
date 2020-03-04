cc31724844259207c5180b2a418be48442488396
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-03-04 15:50:00 +0000

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,38 +1,28 @@
+//clear && gcc -Werror -o 2 2.c -lm && ./2 && rm 2
+
 #include <stdio.h>
 
 int main(){
-int k;
-double sum=1.0,temp,sol,epsilon=1.0;
-int precisao;
+   double sum=0.0,temp,sol,epsilon=1.0;
+   int precisao,k;
 
-printf("intruduza a precisao.\n10^-");
-  scanf("%d",&precisao);
+   printf("intruduza a precisao.\n10^-");
+   scanf("%d",&precisao);
+   for(int i=0; i<precisao; i++) epsilon *=(double) 1.0/10.0;
 
+   for(k=2;;k++){
+      temp = (double) (1.0/(k*k));
+      printf("%.15f\n",temp);
 
-  for(int i=0; i<precisao; i++)
-    epsilon *=(double) 1.0/10.0;
+      if(temp < epsilon) break;
 
+      if(k%2 == 0) sum+=temp;
+      else sum-=temp;
+   }
 
-for(k=2;;k++){
+   printf("\ntermos somados %d\n", k);
 
-    temp = (double) (1.0/(k*k));
-    printf("%.15f\n",temp);
+   sol= sum; //* 12;
 
-    if(temp < epsilon)
-      break;
-
-      else{
-     if(k%2 == 0)
-        sum-=temp;
-        else
-          sum+=temp;
-}
-}
-
-printf("termos somados %d\n", k);
-
-sol=12 * sum;
-
-printf("%.15f\n",sol);
-return 0;
+   printf("%.15f\n",sol);
 }
