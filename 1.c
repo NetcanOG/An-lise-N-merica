afc71f2775185fefc0ae770eda16aa0c3c839ccb
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-03-04 11:54:25 +0000

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,18 +1,21 @@
+//clear && gcc -Werror -o 1 1.c -lm && ./1 && rm 1
+
 #include <stdio.h>
 
 int main(){
    int k=2,precisao;
-   double sol = 9.0,epsilon=1.0;
+   double sol = 9.0,epsilon=1.0,temp;
    
    printf("intruduza a precisao.\n10^-");
    scanf("%d",&precisao);
    for(int i=0; i<precisao; i++) epsilon *=(double) 1/10;
    
-   do {
-      sol *= (double) (k * k) / ((2*k+1)*(2*k+2));
-      k++;
-   } while (sol > epsilon);
+   for(k = 2; ;k++) {
+      temp = (double) sol * (k * k) / ((2*k+1)*(2*k+2));
+      if(temp < epsilon) break;
+      sol = temp;
+   }
    
    printf("\nN de termos somados = %d\n",--k);
-   printf("Valor final = %.*f +- 5*10^-%d\n",precisao-1,sol,precisao);
+   printf("Valor final = %.*f +- 10^-%d\n",precisao+1,sol,precisao);
 }
