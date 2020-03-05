e43c8e6ebfcf6e887d11803cb51b176b906caa2e
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-03-05 23:51:01 +0000

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,27 +1,24 @@
 //clear && gcc -Werror -o 2 2.c -lm && ./2 && rm 2
-//calcula sem underflow 10^-9
 
 #include <stdio.h>
+#include <math.h>
+
+int serie(int precisao){
+   double sum=0.0,temp=0.0,epsilon = pow(0.1,precisao);
+   int k;
+
+   for(k=1;;k++){
+      temp = 12.0/pow(k,2);
+      if(temp < epsilon) break;
+      if(k%2 == 0) sum-=temp;
+      else sum+=temp;
+   }
+   
+   printf("N de termos somados = %d\n",--k);
+   printf("Valor final = %.*f +- 10^-%d\n",precisao+1,sum,precisao);
+   printf("pi^2 =        %.*f\n",precisao+1,M_PI*M_PI);
+}
 
 int main(){
-   double sum=1.0,temp,sol,epsilon=1.0;
-   int precisao,k;
-
-   printf("Introduza a precisao.\n10^-");
-   scanf("%d",&precisao);
-   for(k=0; k<precisao; k++) epsilon *=(double) 1.0/10.0;
-
-   for(k=2;;k++){
-      temp = (double) (1.0/(k*k));
-      if(temp < epsilon) break;
-      //printf("%.15f > %.15f\n",temp,epsilon);
-      if(k%2 == 0) sum+=temp;
-      else sum-=temp;
-   }
-   //printf("%.52f < %.52f\n",temp,epsilon);
-   
-   printf("\nN de termos somados = %d\n",--k);
-
-   sol= sum * 12;
-   printf("Valor final = %.*f +- 10^-%d\n",precisao+1,sol,precisao);
+   for(int i=8;i<=15;i++) serie(i);
 }
