bf5423cb585d4ab98b17ac9762d93365eb4ab705
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-04-01 13:39:12 +0100

    Add files via upload

--- t2ex2b.c
+++ t2ex2b.c
@@ -15,9 +15,9 @@ double df(double x){
 }
 
 void newton(double x0){
-    int iter = 1, precisao = 8;
+    int iter = 1, precisao = 6;
     double x1 = x0 - (f(x0)/df(x0));
-    double erroiter = absoluto(x1-x0), epsilon = 5 * pow(0.1,precisao);
+    double erroiter = absoluto(x1-x0), epsilon = pow(0.1,precisao);
 
     while(erroiter >= epsilon){
         x0 = x1;
