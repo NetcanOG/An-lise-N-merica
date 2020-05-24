6a40174f2618ad002dbe3b2289cbc875680673f0
eduardo010174 <50454358+eduardo010174@users.noreply.github.com>
2020-05-24 17:57:54 +0100

    final ex3

--- t4ex3.c
+++ t4ex3.c
@@ -2,7 +2,8 @@
 #include <math.h>
 
 double a = 0.0, b = 3.0;
-double M_integral_simps = 3385; //calculado externamente
+//maximize fourth derivate sin(cos(sin(cos(x^2)))) over [2.854120,2.854128]
+double M_integral_simps = 3385; //calculado externamente //y = 3384.8
 
 double func(double x){
 	return sin(cos(sin(cos(pow(x,2.0)))));
@@ -19,26 +20,28 @@ unsigned long intervalo_simps(double epsilon){
 		erro = (double)((pow(h,4.0)/180)*(b - a)* M_integral_simps);
 	}
 
-	printf("%li\n",n);
 	return n;
 }
-//maximize fourth derivate sin(cos(sin(cos(x^2)))) over [2.854120,2.854128]
-//3384.8
+
 double integral_simps(double epsilon){
 	unsigned long n = intervalo_simps(epsilon);
 	double h = (double)(b - a)/n;
 	double value = 0.0;
 
 	for(unsigned long i = 1; i <= n-1 ; i++){
-    if(i%2!=0) value += 4*func(a + i*h);
-    else value += 2*func(a + i*h);
-  }
-  value+= func(a)+func(b);
-	return value*h/3;
+		if(i%2!=0) value += 4*func(a + i*h);
+		else value += 2*func(a + i*h);
+	}
+	
+	value += func(a) + func(b);
+	value *= h/3;
+	printf("n = %li\n",n);
+	printf("value = %.15f\n\n", value);
+	return value;
 }
 
 int main(){
-	printf("%.15f\n",integral_simps(pow(10,-5)));
-	printf("%.15f\n",integral_simps(pow(10,-7)));
-	printf("%.15f\n",integral_simps(pow(10,-9)));
+	integral_simps(pow(10,-5);
+	integral_simps(pow(10,-7);
+	integral_simps(pow(10,-9);
 }
